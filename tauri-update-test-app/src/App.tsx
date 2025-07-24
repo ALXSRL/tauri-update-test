ca62e31bc3fa8a7b832edf64d258323439cf7719
ALXS-GitHub <alxs.mnch@gmail.com>
2025-07-24 22:02:19 +0200

    init app

---
+++ tauri-update-test-app/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const VERSION = import.meta.env.VITE_APP_VERSION || 'unknown'
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      <h1>Tauri Update Test App</h1>
+      <h2>Version {VERSION}</h2>
+    </>
+  )
+}
+
+export default App
